200b1f1ad3baf395652b724223c3da6ae5ca725d
MayaLaw <maya_elawrence@yahoo.com>
2024-11-07 23:59:34 -0500

    Official Testing
    Official testing with real sample data.

--- NLTKTesting-Lemmatizers.py
+++ NLTKTesting-Lemmatizers.py
@@ -2,8 +2,8 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
-plurals = plurals = ['caresses', 'flies', 'dies', 'mules', 'defined', 'died', 'dies', 'agrees', 'agrees', 'sizing', 'irrational', 'comical', 'fertilizer']
+words = ['caresses', 'flies', 'dies', 'mules', 'defined', 'died', 'was', 'agrees', 'agrees', 'sizing', 'irrational', 'comical', 'fertilizer']
 
 print("Word Net Lemmatizer: \n")
-for word in plurals:
+for word in words:
     print(f"{word} >>> {lemmatizer.lemmatize(word)}")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NTLKTesting
-Testing NTLK library for better understanding.
+General Description: Testing NTLK library for better understanding.
+
+Diving into different tools and packages within NLTK to analyze what is better to use. Testing packages for different tokenizers, stemmers, and lemmatizers.
